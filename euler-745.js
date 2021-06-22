105b9bdd94e697307ecacf84aab251c17921eb64
Jeremy Banka <hello@jeremybanka.com>
2021-06-22 11:42:26 -0700

    🎉
    Co-authored-by: Vanessa Su <vnessa.su@gmail.com>

---
+++ euler-745.js
@@ -0,0 +1,61 @@
+const x = process.argv[2]
+const y = process.argv[3]
+const z = process.argv[4]
+const n = parseInt(x, 10)
+
+// g(n)
+// For a given number n
+// Find largest square s where n % s === 0
+
+// S(N) run g(n) from n = 1 to n = N, add them all
+
+// S(10)
+// g(1) + g(2) + g(2) ... + g(10)
+// 1 1 1 4 1 1 1 4 9 1
+
+const num = true ? 3 : 4
+
+const checkDivisibilityBySquares = (number, upperLimitSquareRoot) => {
+  let highestSquare = 1
+  while (upperLimitSquareRoot > 1) {
+    const squaredNumber = upperLimitSquareRoot ** 2
+    const remainder = number % squaredNumber
+    if (remainder === 0) {
+      highestSquare = squaredNumber
+      break
+    }
+    --upperLimitSquareRoot
+  }
+  return highestSquare
+}
+
+const checkSquareFactorsOfNumber = number => {
+  const recursiveFactorChecker = upperLimitSquareRoot => {
+    console.log(recursiveFactorChecker)
+    const maybeSquareFactor = upperLimitSquareRoot ** 2
+    const remainder = number % maybeSquareFactor
+    const isSquareFactor = remainder === 0
+    return isSquareFactor
+      ? maybeSquareFactor
+      : () => recursiveFactorChecker(upperLimitSquareRoot - 1)
+  }
+  return recursiveFactorChecker
+}
+
+const getHighestSquareFactor = number => {
+  const root = Math.sqrt(number)
+  const numberIsSquare = root % 1 === 0
+  const recursiveFactorChecker = checkSquareFactorsOfNumber(number)
+  return numberIsSquare
+    ? number
+    : trampoline(recursiveFactorChecker(Math.floor(root)))
+}
+
+const trampoline = recursiveFunction =>
+  ((...args) => {
+    let result = recursiveFunction(...args)
+    while (result.call) result = result()
+    return result
+  })()
+
+console.log(getHighestSquareFactor(n))
---
+++ x.js
@@ -0,0 +1,43 @@
+const x = process.argv[2]
+const y = process.argv[3]
+const z = process.argv[4]
+const n = parseInt(process.argv[2])
+
+const reverse = string =>
+  string === `` ? `` : reverse(string.substring(1)) + string[0]
+
+const reverseSafe = string =>
+  string === `` ? `` : () => reverse(string.substring(1)) + string[0]
+
+const nthTriangleNumberUnsafe = (n, sum = 0) =>
+  n === 0
+    ? sum
+    : nthTriangleNumberUnsafe(n - 1, sum + n)
+
+const nthTriangleNumber = (n, sum = 0) =>
+  n === 0
+    ? sum
+    : () => nthTriangleNumber(n - 1, sum + n)
+const trampoline = recursiveFunction =>
+  ((...args) => {
+    let result = recursiveFunction(...args)
+    while (result.call) result = result()
+    return result
+  })()
+
+const countCoins = valueOfCoin => {
+  const recursiveFunction = (centsRemaining, totalCoins = 0) =>
+    centsRemaining >= valueOfCoin
+      ? () => recursiveFunction(centsRemaining - valueOfCoin, totalCoins++)
+      : totalCoins
+  return recursiveFunction
+}
+
+console.log(trampoline(nthTriangleNumber(n)))
+
+/*
+. . . .
+ . . .
+  . .
+   .
+*/
